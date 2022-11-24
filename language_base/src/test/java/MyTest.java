26a7dc4cc5c21bc7ee6011181215915508f3fe27
“hape” <956396908@qq.com>
2022-11-24 16:19:17 +0800

    单测

---
+++ language_base/src/test/java/MyTest.java
@@ -0,0 +1,20 @@
+import com.ppx.JapanApplication;
+import com.ppx.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @Author: hape
+ * @Date: 2022/11/24 14:07
+ */
+@SpringBootTest(classes = JapanApplication.class)
+public class MyTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Test
+    public void t1(){
+    }
+}
